edb17845b5d4a0477fc42ff74321137632fde22d
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-01-26 11:18:00 -0800

    Add ability to input and display list of employees

--- app.rb
+++ app.rb
@@ -5,3 +5,15 @@ require("sinatra/activerecord")
 require("./lib/employee")
 require("./lib/division")
 require("pg")
+
+get("/") do
+  @employee_list = Employee.all()
+  erb(:main)
+end
+
+post("/employee_post") do
+  employee_name = params.fetch('employee_name')
+  Employee.new({:name => employee_name}).save()
+  @employee_list = Employee.all()
+  erb(:main)
+end
