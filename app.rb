bd3ada408d3c5b7a534fc6891b4d6d742e30ad82
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-01-26 14:30:16 -0800

    Add list-within-list for employees in divisions

--- app.rb
+++ app.rb
@@ -1,20 +1,12 @@
 require("sinatra")
 require("sinatra/reloader")
-also_reload("lib/**/*.rb")
 require("sinatra/activerecord")
+also_reload("lib/**/*.rb")
 require("./lib/employee")
 require("./lib/division")
 require("pg")
 
 get("/") do
-  @employee_list = Employee.all()
-  @division_list = Division.all()
-  erb(:main)
-end
-
-post("/employee_post") do
-  employee_name = params.fetch('employee_name')
-  Employee.new({:name => employee_name}).save()
   @employee_list = Employee.all()
   @division_list = Division.all()
   erb(:main)
@@ -27,3 +19,21 @@ post("/division_post") do
   @division_list = Division.all()
   erb(:main)
 end
+
+post("/employee_post") do
+  employee_name = params.fetch('employee_name')
+  employee_division_id = params.fetch('division_id')
+  Employee.new({:name => employee_name, :division_id => employee_division_id }).save()
+  @employee_list = Employee.all()
+  @division_list = Division.all()
+  erb(:main)
+end
+
+
+# delete("/division_erase") do
+#
+#
+#   @employee_list = Employee.all()
+#   @division_list = Division.all()
+#   erb(:main)
+# end
